4d249b324663476a4c1f00c13d5ebd545ed45166
alex-115 <109692636+alex-115@users.noreply.github.com>
2026-04-12 23:51:37 +0000

    Final data prep and correct setup for modeling

--- Data_prep.py
+++ Data_prep.py
@@ -1,44 +1,23 @@
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import KFold, cross_val_score
-from sklearn.linear_model import LogisticRegression  # or any model
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 
 data = pd.read_csv("Dataset/Hospital_dataset.csv")
 
-# Drop irrelevant column
-data = data.drop("readmission_risk_score", axis=1)
+# Drop irrelevant columns
+data = data.drop(["readmission_risk_score", "admission_date", "patient_id"], axis=1) # No discharge date and we already have season
 
 # Handle missing data
-print(data.isna().sum())
+print(data.isna().sum()) # None
 
-# Type conversions
-print(data.info())
-
-data['admission_date'] = pd.to_datetime(data['admission_date'])
-print(data['admission_date'].dtype)
+# Binary encoding
+data["gender"] = data["gender"].map({"Male": 1, "Female": 0})
 
 # Formatting 
 # Data is formatted correctly
 
 # Variable deriviations
-# None yet, once ramani comes back with findings then I will implement it into our dataset
-
-# Standardization
-#Standardizing predictors is important for models that rely on distances, 
-#gradients, or regularization, because differences in feature scale can distort model behavior. 
-# I can't standardize any data yet until we choose a model, but here is some code just in case
-
-#scaler = StandardScaler()
-
-#numeric_cols = data.select_dtypes(include=['float64', 'int64']).columns
-
-#scaled_arr = scaler.fit_transform(data[numeric_cols])
-
-#scaled_df = pd.DataFrame(scaled_arr, columns=numeric_cols)
-
-#print("Summary of scaled df")
-#scaled_df.info()
+# None done yet.
 
 # Save the new data into a csv
-data.to_csv("prepped_hospital_data.csv", index=False)
+data.to_csv("Dataset/prepped_hospital_data.csv", index=False)
--- Model_1.py
+++ Model_1.py
@@ -1,20 +1,59 @@
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import KFold, cross_val_score
+from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.linear_model import LogisticRegression  # or any model
+from sklearn.pipeline import Pipeline
 
 data = pd.read_csv("Dataset/prepped_hospital_data.csv")
 
-# Methods for training/validation/testing
-# k-fold cross-validation else typical split train: 60-70% -> validation: 15-20% -> test: 15-20%
-X = data.drop("label", axis = 1)
-y = data["label"]
-kf = KFold(n_splits=5, shuffle=True, random_state=42)
-model = LogisticRegression(max_iter=1000)
-scores = cross_val_score(model, X, y, cv=kf, scoring="accuracy")
+# Data preprocessing
+numeric_cols = [
+    "age",
+    "comorbidities_count",
+    "length_of_stay",
+    "medications_count",
+    "followup_visits_last_year",
+    "prev_readmissions"
+]
 
-# For medium 5k - 100k rows training: 80 -> validation: 10 -> test: 10
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, shuffle=True) # I used shuffle to randomize the selection of data
+categorical_cols = [
+    "season",
+    "region",
+    "primary_diagnosis",
+    "treatment_type",
+    "insurance_type",
+    "discharge_disposition"
+]
+preprocess = ColumnTransformer(
+    transformers=[
+        ("cat", OneHotEncoder(handle_unknown="ignore"), categorical_cols),
+        ("num", StandardScaler(), numeric_cols)
+    ], remainder="passthrough"
+)
+
+# Data split 70% training, 15% validation, 15% test
+X = data.drop("label", axis=1)
+y = data["label"]
+
+X_train, X_temp, y_train, y_temp = train_test_split(
+    X, y,
+    test_size=0.30,
+    random_state=42,
+    stratify=y
+)
+
+X_val, X_test, y_val, y_test = train_test_split(
+    X_temp, y_temp,
+    test_size=0.50,
+    random_state=42,
+    stratify=y_temp
+)
+
+# Fit preprocessor on training data and transform all sets
+X_train_transformed = preprocess.fit_transform(X_train)
+X_val_transformed = preprocess.transform(X_val)
+X_test_transformed = preprocess.transform(X_test)
 
 #Training begins here
